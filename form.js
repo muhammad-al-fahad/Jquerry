a04dbbafc8fca9311165f7ee7349ba48f3825566
Muhammad Al Fahad <85945538+muhammad-al-fahad@users.noreply.github.com>
2021-09-29 15:41:43 +0500

    Add files via upload

---
+++ form.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    var fnValidate =  $("#loginForm").validate({
+          rules: {
+              "loginForm-email": {
+                  required: true,
+                  email: true
+              },  
+              "loginForm-password": {
+                  required: true,
+                  minlength: 5
+              } 
+          }
+      });
+
+       $('.btn').click(function(e) {
+          e.preventDefault();
+          if(fnValidate){
+          window.location.reload(true);
+          window.location.href = 'https://kapco.herokuapp.com/';
+          }
+          else{
+              alert('Try Again');
+          }
+      });
+
+  });
